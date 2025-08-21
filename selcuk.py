3ab61f76d63ee0d27518caafe1cab4b56554dc4b
undefinedtv <undefinedtv9@gmail.com>
2025-08-21 18:49:44 +0300

    Update selcuk.py

--- selcuk.py
+++ selcuk.py
@@ -4,24 +4,24 @@ import re
 HEADERS = {"User-Agent": "Mozilla/5.0"}
 
 CHANNELS = [
-    {"id": "selcukbeinsports1", "source_id": "selcukbeinsports1", "name": "BeIN Sports 1 | SS", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/5rhmw31628798883.png", "group": "beIN SPORT"},
-    {"id": "selcukbeinsports2", "source_id": "selcukbeinsports2", "name": "BeIN Sports 2 | SS", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/7uv6x71628799003.png", "group": "beIN SPORT"},
-    {"id": "selcukbeinsports3", "source_id": "selcukbeinsports3", "name": "BeIN Sports 3 | SS", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/u3117i1628798857.png", "group": "beIN SPORT"},
-    {"id": "selcukbeinsports4", "source_id": "selcukbeinsports4", "name": "BeIN Sports 4 | SS", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/2ktmcp1628798841.png", "group": "beIN SPORT"},
-    {"id": "selcukbeinsports5", "source_id": "selcukbeinsports5", "name": "BeIN Sports 5 | SS", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/BeIn_Sports_5_US.png", "group": "beIN SPORT"},
-    {"id": "selcukbeinsportsmax1", "source_id": "selcukbeinsportsmax1", "name": "BeIN Sports Max 1 | SS", "logo": "https://assets.bein.com/mena/sites/3/2015/06/beIN_SPORTS_MAX1_DIGITAL_Mono.png", "group": "beIN SPORT"},
-    {"id": "selcukbeinsportsmax2", "source_id": "selcukbeinsportsmax2", "name": "BeIN Sports Max 2 | SS", "logo": "http://tvprofil.com/img/kanali-logo/beIN_Sports_MAX_2_TR_logo_v2.png?1734011568", "group": "beIN SPORT"},
-    {"id": "selcuktivibuspor1", "source_id": "selcuktivibuspor1", "name": "Tivibu Spor 1 | SS", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/qadnsi1642604437.png", "group": "Tivibu Spor"},
-    {"id": "selcuktivibuspor2", "source_id": "selcuktivibuspor2", "name": "Tivibu Spor 2 | SS", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/kuasdm1642604455.png", "group": "Tivibu Spor"},
-    {"id": "selcuktivibuspor3", "source_id": "selcuktivibuspor3", "name": "Tivibu Spor 3 | SS", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/slwrz41642604502.png", "group": "Tivibu Spor"},
-    {"id": "selcuktivibuspor4", "source_id": "selcuktivibuspor4", "name": "Tivibu Spor 4 | SS", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/59bqi81642604517.png", "group": "Tivibu Spor"},
-    {"id": "selcukssport", "source_id": "selcukssport", "name": "S Sport 1 | SS", "logo": "https://itv224226.tmp.tivibu.com.tr:6430/images/poster/20230302923239.png", "group": "S Spor"},
-    {"id": "selcukssport2", "source_id": "selcukssport2", "name": "S Sport 2 | SS", "logo": "https://itv224226.tmp.tivibu.com.tr:6430/images/poster/20230302923321.png", "group": "S Spor"},
-    {"id": "selcuksmartspor", "source_id": "selcuksmartspor", "name": "Smart Spor 1 | SS", "logo": "https://dsmart-static-v2.ercdn.net//resize-width/1920/content/p/el/11909/Thumbnail.png", "group": "Spor"},
-    {"id": "selcuksmartspor2", "source_id": "selcuksmartspor2", "name": "Smart Spor 2 | SS", "logo": "https://www.dsmart.com.tr/api/v1/public/images/kanallar/SPORSMART2-gri.png", "group": "Spor"},
-    {"id": "selcukaspor", "source_id": "selcukaspor", "name": "A Spor | SS", "logo": "https://feo.kablowebtv.com/resize/168A635D265A4328C2883FB4CD8FF/0/0/Vod/HLS/9d28401f-2d4e-4862-85e2-69773f6f45f4.png", "group": "Spor"},
-    {"id": "selcukeurosport1", "source_id": "selcukeurosport1", "name | SS": "Eurosport 1", "logo": "https://feo.kablowebtv.com/resize/168A635D265A4328C2883FB4CD8FF/0/0/Vod/HLS/54cad412-5f3a-4184-b5fc-d567a5de7160.png", "group": "Spor"},
-    {"id": "selcukeurosport2", "source_id": "selcukeurosport2", "name | SS": "Eurosport 2", "logo": "https://feo.kablowebtv.com/resize/168A635D265A4328C2883FB4CD8FF/0/0/Vod/HLS/a4cbdd15-1509-408f-a108-65b8f88f2066.png", "group": "Spor"},
+    {"id": "bein1", "source_id": "selcukbeinsports1", "name": "BeIN Sports 1", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/5rhmw31628798883.png", "group": "Spor"},
+    {"id": "bein2", "source_id": "selcukbeinsports2", "name": "BeIN Sports 2", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/7uv6x71628799003.png", "group": "Spor"},
+    {"id": "bein3", "source_id": "selcukbeinsports3", "name": "BeIN Sports 3", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/u3117i1628798857.png", "group": "Spor"},
+    {"id": "bein4", "source_id": "selcukbeinsports4", "name": "BeIN Sports 4", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/2ktmcp1628798841.png", "group": "Spor"},
+    {"id": "bein5", "source_id": "selcukbeinsports5", "name": "BeIN Sports 5", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/BeIn_Sports_5_US.png", "group": "Spor"},
+    {"id": "beinmax1", "source_id": "selcukbeinsportsmax1", "name": "BeIN Sports Max 1", "logo": "https://assets.bein.com/mena/sites/3/2015/06/beIN_SPORTS_MAX1_DIGITAL_Mono.png", "group": "Spor"},
+    {"id": "beinmax2", "source_id": "selcukbeinsportsmax2", "name": "BeIN Sports Max 2", "logo": "http://tvprofil.com/img/kanali-logo/beIN_Sports_MAX_2_TR_logo_v2.png?1734011568", "group": "Spor"},
+    {"id": "tivibu1", "source_id": "selcuktivibuspor1", "name": "Tivibu Spor 1", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/qadnsi1642604437.png", "group": "Spor"},
+    {"id": "tivibu2", "source_id": "selcuktivibuspor2", "name": "Tivibu Spor 2", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/kuasdm1642604455.png", "group": "Spor"},
+    {"id": "tivibu3", "source_id": "selcuktivibuspor3", "name": "Tivibu Spor 3", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/slwrz41642604502.png", "group": "Spor"},
+    {"id": "tivibu4", "source_id": "selcuktivibuspor4", "name": "Tivibu Spor 4", "logo": "https://r2.thesportsdb.com/images/media/channel/logo/59bqi81642604517.png", "group": "Spor"},
+    {"id": "ssport1", "source_id": "selcukssport", "name": "S Sport 1", "logo": "https://itv224226.tmp.tivibu.com.tr:6430/images/poster/20230302923239.png", "group": "Spor"},
+    {"id": "ssport2", "source_id": "selcukssport2", "name": "S Sport 2", "logo": "https://itv224226.tmp.tivibu.com.tr:6430/images/poster/20230302923321.png", "group": "Spor"},
+    {"id": "smart1", "source_id": "selcuksmartspor", "name": "Smart Spor 1", "logo": "https://dsmart-static-v2.ercdn.net//resize-width/1920/content/p/el/11909/Thumbnail.png", "group": "Spor"},
+    {"id": "smart2", "source_id": "selcuksmartspor2", "name": "Smart Spor 2", "logo": "https://www.dsmart.com.tr/api/v1/public/images/kanallar/SPORSMART2-gri.png", "group": "Spor"},
+    {"id": "aspor", "source_id": "selcukaspor", "name": "A Spor", "logo": "https://feo.kablowebtv.com/resize/168A635D265A4328C2883FB4CD8FF/0/0/Vod/HLS/9d28401f-2d4e-4862-85e2-69773f6f45f4.png", "group": "Spor"},
+    {"id": "eurosport1", "source_id": "selcukeurosport1", "name": "Eurosport 1", "logo": "https://feo.kablowebtv.com/resize/168A635D265A4328C2883FB4CD8FF/0/0/Vod/HLS/54cad412-5f3a-4184-b5fc-d567a5de7160.png", "group": "Spor"},
+    {"id": "eurosport2", "source_id": "selcukeurosport2", "name": "Eurosport 2", "logo": "https://feo.kablowebtv.com/resize/168A635D265A4328C2883FB4CD8FF/0/0/Vod/HLS/a4cbdd15-1509-408f-a108-65b8f88f2066.png", "group": "Spor"},
 ]
 
 def find_working_domain(start=6, end=100):
@@ -63,58 +63,15 @@ def fetch_streams(domain, referer):
     return result
 
 def write_m3u(links, filename="selcuk.m3u", referer=""):
-    print(f"\nM3U dosyası güncelleniyor: {filename}")
-
-    # Mevcut içeriği oku
-    try:
-        with open(filename, "r", encoding="utf-8") as f:
-            original_lines = f.read().splitlines()
-    except FileNotFoundError:
-        original_lines = ["#EXTM3U"]
-
-    updated_lines = []
-    i = 0
-    while i < len(original_lines):
-        line = original_lines[i]
-
-        if line.startswith("#EXTINF:"):
-            match = re.search(r'tvg-id="([^"]+)"', line)
-            if match:
-                tvg_id = match.group(1)
-                updated = False
-                for ch, url in links:
-                    if ch["id"] == tvg_id:
-                        # Kanal bilgilerini güncelle
-                        updated_lines.append(f'#EXTINF:-1 tvg-id="{ch["id"]}" tvg-name="{ch["name"]}" tvg-logo="{ch["logo"]}" group-title="{ch["group"]}",{ch["name"]}')
-                        updated_lines.append(f"#EXTVLCOPT:http-referrer={referer}")
-                        updated_lines.append(url)
-                        updated = True
-                        break
-                if updated:
-                    i += 3  # Mevcut kanalın tüm satırlarını atla
-                    continue
-        updated_lines.append(line)
-        i += 1
-
-    # Mevcut dosyada olmayan yeni kanalları sona ekle
-    existing_ids = set()
-    for l in updated_lines:
-        if l.startswith("#EXTINF:") and 'tvg-id=' in l:
-            match = re.search(r'tvg-id="([^"]+)"', l)
-            if match:
-                existing_ids.add(match.group(1))
-
+    print(f"\n M3U dosyası yazılıyor: {filename}")
+    lines = ["#EXTM3U"]
     for ch, url in links:
-        if ch["id"] not in existing_ids:
-            updated_lines.append(f'#EXTINF:-1 tvg-id="{ch["id"]}" tvg-name="{ch["name"]}" tvg-logo="{ch["logo"]}" group-title="{ch["group"]}",{ch["name"]}')
-            updated_lines.append(f"#EXTVLCOPT:http-referrer={referer}")
-            updated_lines.append(url)
-
-    # Dosyayı yeniden yaz
+        lines.append(f'#EXTINF:-1 tvg-id="{ch["id"]}" tvg-name="{ch["name"]}" tvg-logo="{ch["logo"]}" group-title="{ch["group"]}",{ch["name"]}')
+        lines.append(f"#EXTVLCOPT:http-referrer={referer}")
+        lines.append(url)
     with open(filename, "w", encoding="utf-8") as f:
-        f.write("\n".join(updated_lines))
-
-    print("Güncelleme tamamlandı. Güncellenen kanal sayısı:", len(links))
+        f.write("\n".join(lines))
+    print(" Tamamlandı. Kanal sayısı:", len(links))
 
 def main():
     html, referer = find_working_domain()
@@ -127,9 +84,7 @@ def main():
     print(f"Yayın domaini: {stream_domain}")
     streams = fetch_streams(stream_domain, referer)
     if streams:
-
         write_m3u(streams, referer=referer)
-        #write_m3u(streams, filename="1.m3u", referer=referer)
     else:
         print("Hiçbir yayın alınamadı.")
 
