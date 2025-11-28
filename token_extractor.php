32dca1706bc8e83055ed757f3ebe31e92b5ca124
undefinedtv <undefinedtv9@gmail.com>
2025-11-28 21:32:26 +0300

    Update token_extractor.php

--- token_extractor.php
+++ token_extractor.php
@@ -6,7 +6,7 @@ $headers = [
 ];
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_URL, $url . "?checkip=false");
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
