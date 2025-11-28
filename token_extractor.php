7941243aa1fbb5e13e57ffb36fa4d2c8bf2dde03
undefinedtv <undefinedtv9@gmail.com>
2025-11-28 20:28:38 +0300

    Create token_extractor.php

---
+++ token_extractor.php
@@ -0,0 +1,30 @@
+<?php
+$url = "https://core-api.kablowebtv.com/api/channels";
+$headers = [
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+    "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnYiOiJMSVZFIiwiaXBiIjoiMCIsImNnZCI6IjA5M2Q3MjBhLTUwMmMtNDFlZC1hODBmLTJiODE2OTg0ZmI5NSIsImNzaCI6IlRSS1NUIiwiZGN0IjoiM0VGNzUiLCJkaSI6IjMwYTM5YzllLWE4ZDYtNGEwMC05NDBmLTFjMTE4NDgzZDcxMiIsInNnZCI6ImJkNmUyNmY5LWJkMzYtNDE2ZC05YWQzLTYzNjhlNGZkYTMyMiIsInNwZ2QiOiJjYjZmZGMwMi1iOGJlLTQ3MTYtYTZjYi1iZTEyYTg4YjdmMDkiLCJpY2giOiIwIiwiaWRtIjoiMCIsImlhIjoiOjpmZmZmOjEwLjAuMC4yMDYiLCJhcHYiOiIxLjAuMCIsImFibiI6IjEwMDAiLCJuYmYiOjE3NTE3MDMxODQsImV4cCI6MTc1MTcwMzI0NCwiaWF0IjoxNzUxNzAzMTg0fQ.SGC_FfT7cU1RVM4E5rMYO2IsA4aYUoYq2SXl51-PZwM"
+];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+
+$data = json_decode($response, true);
+
+if (!$data || !isset($data['Data']['AllChannels'][0]['StreamData']['HlsStreamUrl'])) {
+    die('API yanıtı geçersiz veya kanal bulunamadı');
+}
+
+// Token'ı stream URL'sinden çıkar
+$streamUrl = $data['Data']['AllChannels'][0]['StreamData']['HlsStreamUrl'];
+if (preg_match('/wmsAuthSign=(.*?)(?:$|&)/', $streamUrl, $matches)) {
+    $token = $matches[1];
+    file_put_contents('token.txt', $token);
+    echo "Token güncellendi: " . substr($token, 0, 20) . "...";
+} else {
+    die('Token bulunamadı!');
+}
+?>
